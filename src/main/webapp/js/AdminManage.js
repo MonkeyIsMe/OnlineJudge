0defe27069d7f5e7e2c1b77f6184eb027d33606b
CallMeDad <CallMeDad@LAPTOP-FOL3OPE7>
2019-08-01 08:58:41 +0800

    整合部分网页

---
+++ src/main/webapp/js/AdminManage.js
@@ -0,0 +1,53 @@
+
+
+$(document).ready(function(){
+
+    $("#SetAd").hover(function(){
+        $("#SetAd").css("background-color","#FF7E00");
+    },function(){
+        $("#SetAd").css("background-color","#c0c0c0");
+
+    });
+
+    $("#UserList").hover(function(){
+        $("#UserList").css("background-color","#FF7E00");
+    },function(){
+        $("#UserList").css("background-color","#c0c0c0");
+
+    });
+
+    $("#ProblemList").hover(function(){
+        $("#ProblemList").css("background-color","#FF7E00");
+    },function(){
+        $("#ProblemList").css("background-color","#c0c0c0");
+
+    });
+
+    $("#AddHomework").hover(function(){
+        $("#AddHomework").css("background-color","#FF7E00");
+    },function(){
+        $("#AddHomework").css("background-color","#c0c0c0");
+
+    });
+
+    $("#ProblemManage").hover(function(){
+        $("#ProblemManage").css("background-color","#FF7E00");
+    },function(){
+        $("#ProblemManage").css("background-color","#c0c0c0");
+
+    });
+
+    $("#RoleManage").hover(function(){
+        $("#RoleManage").css("background-color","#FF7E00");
+    },function(){
+        $("#RoleManage").css("background-color","#c0c0c0");
+
+    });
+
+    $("#HomeworkList").hover(function(){
+        $("#HomeworkList").css("background-color","#FF7E00");
+    },function(){
+        $("#HomeworkList").css("background-color","#c0c0c0");
+
+    });
+});
---
+++ src/main/webapp/js/Compile.js
@@ -0,0 +1,142 @@
+$(document).ready(function(){
+    $("#head").load('MainHead.html');
+})
+
+
+let first; //存放一个选择框
+var array = [];
+
+var choosed;
+var i = 0;
+
+//编译器
+ace.require("ace/ext/language_tools");
+var editor = ace.edit("editor");
+editor.setTheme("ace/theme/dreamweaver");
+editor.session.setMode("ace/mode/java");
+editor.setShowPrintMargin(true);
+editor.session.getLength();
+editor.session.setUseWrapMode(true);
+editor.setOptions({
+    enableBasicAutocompletion: true,
+    enableSnippets: true,
+    enableLiveAutocompletion: true,//只能补全
+});
+
+
+//对主题的切换
+$("#ambiance").click(function () {
+    editor.setTheme("ace/theme/ambiance");
+    $("#theme").text("ambiance");
+})
+
+
+$("#chaos").click(function () {
+    editor.setTheme("ace/theme/chaos");
+    $("#theme").text("chaos");
+})
+
+$("#chrome").click(function () {
+    editor.setTheme("ace/theme/chrome");
+    $("#theme").text("chrome");
+})
+
+$("#dracula").click(function () {
+    editor.setTheme("ace/theme/dracula");
+    $("#theme").text("dracula");
+})
+
+$("#dreamweaver").click(function () {
+    editor.setTheme("ace/theme/dreamweaver");
+    $("#theme").text("dreamweaver");
+})
+
+$("#eclipse").click(function () {
+    editor.setTheme("ace/theme/eclipse");
+    $("#theme").text("eclipse");
+})
+
+$("#xcode").click(function () {
+    editor.setTheme("ace/theme/xcode");
+    $("#theme").text("xcode");
+})
+
+
+$("#terminal").click(function () {
+    editor.setTheme("ace/theme/terminal");
+    $("#theme").text("terminal");
+})
+
+//字体大小设置
+function fontsize(size) {
+    $("#editor").css("font-size",size);
+}
+
+function ChooseLang(ch) {
+    choosed = ch;
+    //console.log(ch);
+}
+
+$("#CreateCode").click(function(){
+    //alert(choosed)
+    var filename = $("#CodeName").val();
+    var language = choosed;
+    var lang;
+    if(language == 'C++' || language == 'C'){
+        lang = 'cpp'
+    }
+    if(language == 'Java'){
+        lang = 'class'
+    }
+    if(language == 'Python'){
+        lang = 'py'
+    }
+    if(i == 0){
+        var html ="<div class='file'><input id='"+filename+"' type='radio' value='" + filename + "' checked=\"checked\" name='code' onclick=inputcode('" + filename + "')>"+filename+"."+lang+"</div>";
+        first = filename;
+        var arr ={
+            key:filename,
+            value:""
+        }
+        array.push(arr)
+        i++;
+    }
+    else{
+        var html ="<div class='file'><input id='"+filename+"' type='radio' value='" + filename + "' name='code' onclick=inputcode('" + filename + "')>"+filename+"."+lang+"</div>";
+        var arr ={
+            key:filename,
+            value:""
+        }
+        array.push(arr)
+    }
+
+
+    $("#content").append(html);
+})
+
+$("#clear").click(function () {
+    editor.setValue("");
+})
+
+
+function inputcode(filename) {
+
+    var firstcode = editor.getValue();
+    for(var j in array){
+        if(array[j].key == first){
+            array[j].value = firstcode;
+        }
+    }
+
+    first = filename;
+    var secondcode = "";
+    for(var k in array){
+        if(array[k].key == first){
+            secondcode = array[k].value;
+        }
+    }
+
+    if(secondcode == null) secondcode ="";
+    editor.setValue(secondcode);
+
+}
