a09c137414e61cc97cdb0e713354ff7f3fbcaef3
CallMeDad <CallMeDad@LAPTOP-FOL3OPE7>
2019-08-09 18:09:25 +0800

    管理端工作界面数据传输

--- src/main/webapp/js/SingleWork.js
+++ src/main/webapp/js/SingleWork.js
@@ -14,6 +14,18 @@ $(function(){
 			function(data) {
 				var datas = JSON.parse(data);
 				console.log(datas);
+				$("#update_name").val(datas.workName);
+				$("#update_owner").val(datas.workOwner);
+				$("#update_begin").val(datas.workBeginTime);
+				$("#update_end").val(datas.workEndTime);
+				$("#update_create").val(datas.workCreatTime);
+				$("#update_info").append(datas.workInfo);
+				if(datas.workFlag == 1){
+					$("input[name=work][value=exam]").attr("checked",true);
+				}
+				else {
+					$("input[name=work][value=homework]").attr("checked",true);
+				}
 			}
 	);
 	
