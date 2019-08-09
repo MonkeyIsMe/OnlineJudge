f8bdce3da772f1948f624b7949257d1995cb3b66
CallMeDad <CallMeDad@LAPTOP-FOL3OPE7>
2019-08-09 14:35:54 +0800

    初试完成管理端单一工作界面的信息

--- src/main/webapp/js/SingleWork.js
+++ src/main/webapp/js/SingleWork.js
@@ -2,3 +2,18 @@ var url = decodeURI(window.location.href);
  
 var argsIndex = url .split("?WorkId=");
 var wid = argsIndex[1];
+
+
+$(function(){
+	$.post(
+			"QuerySingleWork.action",
+			{
+				work_id:wid,
+			}, 
+			function(data) {
+				var datas = JSON.parse(data);
+				//console.log(datas);
+			}
+	);
+	
+});
