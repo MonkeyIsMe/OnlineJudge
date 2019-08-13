29c611401201dc60bd28d06e264c36d9f6cebf86
CallMeDad <CallMeDad@LAPTOP-FOL3OPE7>
2019-08-13 15:53:08 +0800

    题目样例的更新

--- src/main/webapp/js/ProblemCase.js
+++ src/main/webapp/js/ProblemCase.js
@@ -9,8 +9,11 @@ var pid = argsIndex[1];
 
 
 
-var cid; //记录第几行的编号
 
+var cid; //记录第几行的编号
+var cflag //记录第几行的标志位
+var cin; //记录第几行的输入
+var cout; //记录第几行的输出
 
 $(function(){
     $.post(
@@ -50,11 +53,19 @@ $(document).ready(function(){
 	  //用于读取所选表行单元格数据（值）的代码
 	  $("#myTable").on('click','.delete',function(){
 	    //获得当前行
-	    var currentRow=$(this).closest("tr"); 
-	    var col1=currentRow.find("td:eq(0)").text(); //获得当前行第一个TD值
+	    var currentRow = $(this).closest("tr"); 
+	    var col1 = currentRow.find("td:eq(0)").text(); //获得当前行第一个TD值
+	    var col2 = currentRow.find("td:eq(1)").text(); //获得当前行第一个TD值
+	    var col3 = currentRow.find("td:eq(2)").text(); //获得当前行第一个TD值
+	    var col4 = currentRow.find("td:eq(3)").text(); //获得当前行第一个TD值
 	    
 	    cid = col1;
+	    cflag = col2;
+	    cin = col3;
+	    cout = col4;
 	    
+	    $("#update_case_input").append(cin);
+	    $("#update_case_output").append(cout);
 	    
 	  });
 	  
@@ -65,6 +76,37 @@ $(document).ready(function(){
 					"DeleteCase.action",
 					{
 						case_id:cid,
+					},
+					function(data){
+						data = data.replace(/^\s*/, "").replace(/\s*$/, "");
+					    pid = col1;
+					    var url = "ProblemCase.html?ProblemId=" + pid;
+						if(data == "Fail"){
+							alert("删除失败！");
+							window.location.replace(url);
+						}
+						else{
+							alert("删除成功!");
+							window.location.replace(url);
+						}
+					}
+					);
+		})
+		
+		$("#update_case").click(function(){
+			//console.log(cid);
+			
+			var input = $("#update_case_input").val();
+			var output = $("#update_case_output").val();
+			
+			$.post(
+					"UpdateCase.action",
+					{
+						case_id:cid,
+						case_flag:cflag,
+						problem_id:pid,
+						case_input:input,
+						case_output:output
 					},
 					function(data){
 						data = data.replace(/^\s*/, "").replace(/\s*$/, "");
