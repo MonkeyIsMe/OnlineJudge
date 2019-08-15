b0f7222554ec9c7bbfbd108c7d24e205de43c618
CallMeDad <CallMeDad@LAPTOP-FOL3OPE7>
2019-08-15 20:11:04 +0800

    细节更新，单一题目界面初步

--- src/main/webapp/js/ProblemSet.js
+++ src/main/webapp/js/ProblemSet.js
@@ -114,8 +114,9 @@ function PrevPage(){
 				        $trTemp.append("<td style=" + "text-align:center"  + ">" +data[i].know  +"</td>");
 				        $trTemp.append("<td style=" + "text-align:center"  + ">" +data[i].people  +"</td>");
 				        $trTemp.append("<td style=" + "text-align:center"  + ">" + 
-				        		'<a id="pic"><span class="delete glyphicon glyphicon-pencil" style="cursor:pointer;margin-left:30px" data-toggle="modal" data-target="#update_Modal"></span></a>'
-				        		+'<a id="pic"><span  class="delete glyphicon glyphicon-trash" style="cursor:pointer;margin-left:20px" data-toggle="modal" data-target="#myModal"></span></a>'
+				        		'<a><span class="delete glyphicon glyphicon-pencil" style="cursor:pointer;margin-left:50px" data-toggle="modal" data-target="#update_Modal"></span></a>'
+				        		+'<a><span  class="delete cases glyphicon glyphicon-tasks" style="cursor:pointer;margin-left:20px" ></span></a>'
+				        		+'<a><span  class="delete glyphicon glyphicon-trash" style="cursor:pointer;margin-left:20px" data-toggle="modal" data-target="#myModal"></span></a>'
 				        		+"</td>");
 	                    // $("#J_TbData").append($trTemp);
 	                    $trTemp.appendTo("#KnowList");
@@ -154,8 +155,9 @@ function NextPage(){
 	    			        $trTemp.append("<td style=" + "text-align:center"  + ">" +data[i].know  +"</td>");
 	    			        $trTemp.append("<td style=" + "text-align:center"  + ">" +data[i].people  +"</td>");
 	    			        $trTemp.append("<td style=" + "text-align:center"  + ">" + 
-	    			        		'<a id="pic"><span class="delete glyphicon glyphicon-pencil" style="cursor:pointer;margin-left:30px" data-toggle="modal" data-target="#update_Modal"></span></a>'
-	    			        		+'<a id="pic"><span  class="delete glyphicon glyphicon-trash" style="cursor:pointer;margin-left:20px" data-toggle="modal" data-target="#myModal"></span></a>'
+	    			        		'<a><span class="delete glyphicon glyphicon-pencil" style="cursor:pointer;margin-left:50px" data-toggle="modal" data-target="#update_Modal"></span></a>'
+	    			        		+'<a><span  class="delete cases glyphicon glyphicon-tasks" style="cursor:pointer;margin-left:20px" ></span></a>'
+	    			        		+'<a><span  class="delete glyphicon glyphicon-trash" style="cursor:pointer;margin-left:20px" data-toggle="modal" data-target="#myModal"></span></a>'
 	    			        		+"</td>");
 	                        // $("#J_TbData").append($trTemp);
 	                        $trTemp.appendTo("#KnowList");
@@ -187,7 +189,7 @@ $(document).ready(function(){
 		    
 		    pid = col1;
 		    var url = "ProblemCase.html?ProblemId=" + pid;
-		    window.open(url);
+		    window.location.replace(url);
 		    
 		  });
 	  
