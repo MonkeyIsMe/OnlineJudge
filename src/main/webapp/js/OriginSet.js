bce0324b70c1ac7971de5b7aacf10caef65955fb
CallMeDad <CallMeDad@DESKTOP-85NT2JK>
2019-08-29 09:41:55 +0800

    细节更新，题目的标程界面更新

--- src/main/webapp/js/OriginSet.js
+++ src/main/webapp/js/OriginSet.js
@@ -3,6 +3,7 @@ var url = decodeURI(window.location.href);
 var argsIndex = url .split("?pid=");
 var pid = argsIndex[1];
 
+var oid;
 
 $(function(){
     $.post(
@@ -22,8 +23,8 @@ $(function(){
 			        $trTemp.append("<td style=" + "text-align:center"  + ">" +data[i].problemId  +"</td>");
 			        $trTemp.append("<td style=" + "text-align:center"  + ">" +data[i].problemName  +"</td>");
 			        $trTemp.append("<td style=" + "text-align:center"  + ">" + 
-			        		'<a><span class="delete glyphicon glyphicon-pencil" style="cursor:pointer;margin-left:40%" data-toggle="modal" data-target="#update_Modal"></span></a>'
-			        		+'<a><span class="delete glyphicon glyphicon-minus" style="cursor:pointer;margin-left:10%" data-toggle="modal" data-target="#update_Modal"></span></a>'
+			        		'<a><span class="updatecode glyphicon glyphicon-pencil" style="cursor:pointer;margin-left:2%" data-toggle="modal" data-target="#myModal"></span></a>'
+			        		+'<a><span class="deletecode glyphicon glyphicon-minus" style="cursor:pointer;margin-left:10%" data-toggle="modal" data-target="#del_Modal"></span></a>'
 			        		+"</td>");
                     // $("#J_TbData").append($trTemp);
                     $trTemp.appendTo("#KnowList");
@@ -33,6 +34,25 @@ $(function(){
 
 });
 
+$(document).ready(function(){
+	  //用于读取所选表行单元格数据（值）的代码
+	  $("#myTable").on('click','.deletecode',function(){
+	    //获得当前行
+	    var currentRow=$(this).closest("tr"); 
+	    var col1=currentRow.find("td:eq(0)").text(); //获得当前行第一个TD值
+	    
+	    oid = col1;
+	    	    
+	  });
+	  
+		
+		
+		
+});
+
+function BackPrev(){
+	window.location.replace("ManagerCode.html");
+}
 
 function refresh(){
 	window.location.replace("OriginCode.html");
