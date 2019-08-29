c03f0ebb6112b218a612af1dba0457a610f563ae
CallMeDad <CallMeDad@DESKTOP-85NT2JK>
2019-08-29 14:58:28 +0800

    更新一个标程

--- src/main/webapp/js/OriginSet.js
+++ src/main/webapp/js/OriginSet.js
@@ -5,6 +5,19 @@ var pid = argsIndex[1];
 
 var oid;
 
+ace.require("ace/ext/language_tools");
+var editor = ace.edit("editor");
+editor.setTheme("ace/theme/dreamweaver");
+editor.session.setMode("ace/mode/java");
+editor.setShowPrintMargin(true);
+editor.session.getLength();
+editor.session.setUseWrapMode(true);
+editor.setOptions({
+    enableBasicAutocompletion: true,
+    enableSnippets: true,
+    enableLiveAutocompletion: true,//只能补全
+});
+
 $(function(){
     $.post(
         "QueryOriginByProblemId.action",
@@ -13,7 +26,7 @@ $(function(){
         },
         function(data) {
             var data = JSON.parse(data);
-            console.log(data);
+            //console.log(data);
                 for( var i = 0; i < data.length; i++ ) {
                     //动态创建一个tr行标签,并且转换成jQuery对象
                     var $trTemp = $("<tr ></tr>");
@@ -50,15 +63,29 @@ $(document).ready(function(){
 		    //获得当前行
 		    var currentRow=$(this).closest("tr"); 
 		    var col1=currentRow.find("td:eq(0)").text(); //获得当前行第一个TD值
+		    var col2=currentRow.find("td:eq(1)").text();
+		    
+		    oid = col1;
+		    
+		    if(col2 == "Java"){
+		    	editor.session.setMode("ace/mode/java");
+		    }
+		    else if(col2 =="C" || col2 == "C++"){
+		    	editor.session.setMode("ace/mode/c_cpp");
+		    }
+		    else{
+		    	editor.session.setMode("ace/mode/python");
+		    }
 		    
 			$.post(
-					"QueryOriginByProblemId.action",
+					"QuerySingleOrigin.action",
 					{
-						origin_id:col1,
+						origin_id:oid,
 					},
 					function(data){
 						var data = JSON.parse(data);
-						console.log(data);
+						//console.log(data);
+						editor.setValue(data.problemCode);
 					}
 					);
 		    	    
@@ -85,6 +112,35 @@ $(document).ready(function(){
 					);
 		})
 		
+		$("#update").click(function(){
+			//console.log(cid);
+			
+			var codeContent = editor.getValue();
+			var options = $("#select_lang option:selected").val();
+			
+			//console.log(codeContent);
+			//console.log(options);
+			$.post(
+					"UpdateOrigin.action",
+					{
+						origin_id:cid,
+						origin_code:codeContent,
+						origin_type:options,
+					},
+					function(data){
+						data = data.replace(/^\s*/, "").replace(/\s*$/, "");
+						if(data == "Fail"){
+							alert("更新失败！");
+							window.location.replace("OriginCode.html");
+						}
+						else{
+							alert("更新成功!");
+							window.location.replace("OriginCode.html");
+						}
+					}
+					);
+
+		})
 
 		
 		
