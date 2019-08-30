6646a50bed2da05a1e102a57d7b11507074fbba1
CallMeDad <CallMeDad@DESKTOP-85NT2JK>
2019-08-30 19:43:25 +0800

    作业-考试列表的分页，管理端菜单的更新，细节更新

--- src/main/webapp/js/Work.js
+++ src/main/webapp/js/Work.js
@@ -1,6 +1,25 @@
 var row = 1;
 
 $(function(){
+	$.ajaxSettings.async = false;
+	$.post(
+			"CountWork.action",
+			{
+			},
+			function(data){
+				var data = JSON.parse(data);
+				//console.log(data);
+				var sum = data.WorkCount;
+				count = Math.ceil(sum/25);
+				var total = "共" + Math.ceil(sum/25) + "页";
+				$("#TotalPage").append(total);
+				$("#NowPage").append("，当前第" + row + "页");
+			}
+			);
+	
+});
+
+$(function(){
 	$.post(
 			"QueryAllWork.action",
 			{
@@ -15,10 +34,10 @@ $(function(){
 				        //动态创建一个tr行标签,并且转换成jQuery对象
 				        var $trTemp = $("<tr ></tr>");
 				        //往行里面追加 td单元格
-				        $trTemp.append("<td style=" + "text-align:center"  + ">"+ data[i].workId +"</td>");
-				        $trTemp.append("<td onclick=SeeProblem('"+data[i].workId+"') style=" + "text-align:center;color:white"  + ">"  +data[i].workName +"</td>");
-				        $trTemp.append("<td style=" + "text-align:center"  + ">" +data[i].workOwner  +"</td>");
-				        $trTemp.append("<td style=" + "text-align:center"  + ">" + data[i].workCreatTime+"</td>");
+				        $trTemp.append("<td style=" + "text-align:center;font-size:16px"  + ">"+ data[i].workId +"</td>");
+				        $trTemp.append("<td onclick=SeeProblem('"+data[i].workId+"') style=" + "text-align:center;font-size:16px"  + ">"  +data[i].workName +"</td>");
+				        $trTemp.append("<td style=" + "text-align:center;font-size:16px"  + ">" +data[i].workOwner  +"</td>");
+				        $trTemp.append("<td style=" + "text-align:center;font-size:16px"  + ">" + data[i].workCreatTime+"</td>");
 				        // $("#J_TbData").append($trTemp);
 				        $trTemp.appendTo("#WorkList");
 				    }
@@ -27,6 +46,75 @@ $(function(){
 	);
 	
 });
+
+function PrevPage(){	
+	if(row == 1){
+		alert("没有前一页了");
+	}
+	else{
+		row--;
+		$("#KnowList").html("");
+		$.post(
+				"QueryAllWork.action",
+				{
+					page:row,
+					limit:30
+				}, 
+				function(data) {
+					var data = JSON.parse(data);
+					//console.log(data);
+					for(var i = 0 ; i < data.length ; i ++){
+					    for( var i = 0; i < data.length; i++ ) {
+					        //动态创建一个tr行标签,并且转换成jQuery对象
+					        var $trTemp = $("<tr ></tr>");
+					        //往行里面追加 td单元格
+					        $trTemp.append("<td style=" + "text-align:center;font-size:16px"  + ">"+ data[i].workId +"</td>");
+					        $trTemp.append("<td onclick=SeeProblem('"+data[i].workId+"') style=" + "text-align:center;font-size:16px"  + ">"  +data[i].workName +"</td>");
+					        $trTemp.append("<td style=" + "text-align:center;font-size:16px"  + ">" +data[i].workOwner  +"</td>");
+					        $trTemp.append("<td style=" + "text-align:center;font-size:16px"  + ">" + data[i].workCreatTime+"</td>");
+					        // $("#J_TbData").append($trTemp);
+					        $trTemp.appendTo("#WorkList");
+					    }
+					}
+			}
+		);
+	}
+}
+
+function NextPage(){
+	if(row == count){
+		alert("没有后一页了");
+	}
+	else{
+		row ++;
+		$("#KnowList").html("");
+		$.post(
+				"QueryAllWork.action",
+				{
+					page:row,
+					limit:30
+				}, 
+				function(data) {
+					var data = JSON.parse(data);
+					//console.log(data);
+					for(var i = 0 ; i < data.length ; i ++){
+					    for( var i = 0; i < data.length; i++ ) {
+					        //动态创建一个tr行标签,并且转换成jQuery对象
+					        var $trTemp = $("<tr ></tr>");
+					        //往行里面追加 td单元格
+					        $trTemp.append("<td style=" + "text-align:center;font-size:16px"  + ">"+ data[i].workId +"</td>");
+					        $trTemp.append("<td onclick=SeeProblem('"+data[i].workId+"') style=" + "text-align:center;font-size:16px"  + ">"  +data[i].workName +"</td>");
+					        $trTemp.append("<td style=" + "text-align:center;font-size:16px"  + ">" +data[i].workOwner  +"</td>");
+					        $trTemp.append("<td style=" + "text-align:center;font-size:16px"  + ">" + data[i].workCreatTime+"</td>");
+					        // $("#J_TbData").append($trTemp);
+					        $trTemp.appendTo("#WorkList");
+					    }
+					}
+			}
+		);
+	}
+
+}
 
 $(function(){
 	$.post(
